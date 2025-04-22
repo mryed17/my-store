a1bf6eb27a4c747fc974db320b9c79bd6f75969d
mryed17 <merryantiedyanto17@gmail.com>
2025-04-23 01:33:18 +0700

    layout.tsx

--- app/buyer/layout.tsx
+++ app/buyer/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "../globals.css";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={font.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
