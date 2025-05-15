3394d07dd96c248e07fa38376da3aeec531f5be0
yesaya1011 <flamelightning1@gmail.com>
2025-05-15 20:23:04 +0700

    n

--- app/buyer/layout.tsx
+++ app/buyer/layout.tsx
@@ -1,28 +1,16 @@
-import type { Metadata } from "next";
-import { Urbanist } from "next/font/google";
-import "../globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
-const font = Urbanist({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Toko Online",
-  description: "Toko Online",
-};
-
-export default function RootLayout({
+export default function BuyerLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={font.className}>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
   );
 }
